16a900da01be2a2a23e838f188276da2ddcfb868
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2023-07-11 09:50:03 -0300

    ADD: Adicionado Telas

---
+++ telas/deletarCategoria.php
@@ -0,0 +1,10 @@
+<?php
+    require_once '../classes/Sistema.php';
+
+    $codCateg = $_REQUEST['codCateg'];
+    $sistema = new Sistema();
+    $sistema->deletarCat($codCateg);
+?>
+<script>
+    window.location.href = "../index.php"
+</script>
