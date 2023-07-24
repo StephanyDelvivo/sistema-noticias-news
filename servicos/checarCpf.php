2f44df952e22fcc133af1fd91b871a52909ab977
GGMagenta <ggmagenta@gmail.com>
2023-07-24 16:48:19 -0300

    cad assinante e jornalista checa se cpf ja existe

---
+++ servicos/checarCpf.php
@@ -0,0 +1,20 @@
+<?php
+    require_once '../classes/Sistema.php';
+
+    function checarCpf($cpf){
+        $sistema = new Sistema();
+        $usuario = $sistema->buscarAss($cpf);
+        if(!isset($usuario)){
+            $usuario = $sistema->buscarJorn($cpf);
+            if(!isset($usuario)){
+                $usuario = $sistema->buscarAdmin($cpf);
+            }
+        }
+        if(isset($usuario)){
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+    
+?>
--- telas/cadJornalista.php
+++ telas/cadJornalista.php
@@ -1,5 +1,6 @@
 <?php
     include '../classes/Sistema.php';
+    include '../servicos/checarCpf.php';
 
     $nome = $_REQUEST['nome'];
     $cpf = $_REQUEST['cpf'];
@@ -10,8 +11,16 @@
     $jorn = new Jornalista($nome, $cpf, $email, $senha, $histJorn);
     $sistema = new Sistema();
 
-    $sistema->cadastrarJorn($jorn);
+    if(checarCpf($cpf)){
+        $mensagem = 'O CPF já existe no sistema.';
+        $link = './formCadJornalista.php';
+    } else {
+        $mensagem = 'Jornalista cadastrado com sucesso!';
+        $link = './index.php';
+        $sistema->cadastrarJorn($jorn);
+    }
 ?>
 <script>
-    window.location.href = "./index.php"
+    alert('<?=$mensagem?>');
+    window.location.href = "<?=$link?>"
 </script>
