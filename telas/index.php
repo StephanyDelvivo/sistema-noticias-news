5bf9ebffddfa51d7d48d8276fe47e2f7a0e6cd58
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2023-07-25 09:18:26 -0300

    .

--- telas/index.php
+++ telas/index.php
@@ -16,6 +16,45 @@
     include './cabecalho.php';
     ?>
     <?php
+    ?>
+    <div class="carrosel">
+        <div class="fotosNoticia">
+            <div>
+                <input type="radio" name="radio-btn" id="radio1">
+                <input type="radio" name="radio-btn" id="radio2">
+                <input type="radio" name="radio-btn" id="radio3">
+                <input type="radio" name="radio-btn" id="radio4">
+            </div>
+
+            <div class="fotoCarrosel" >
+                <img src="../imagem/politica.jpg" alt="foto1">
+            </div>
+            <div>
+                <img class="fotoCarrosel" src="../imagem/saude.png" alt="foto2">
+            </div>
+            <div>
+                <img class="fotoCarrosel" src="../imagem/tecnologia1.jpeg" alt="foto3">
+            </div>
+            <div>
+                <img class="fotoCarrosel" src="../imagem/economia.jpg" alt="foto4">
+            </div>  
+
+            <div class="navegador">
+                <div class="btn1"></div>
+                <div class="btn2"></div>
+                <div class="btn3"></div>
+                <div class="btn4"></div>
+            </div>
+        </div> 
+
+        <div class="manual-navegador">
+            <label for="radio1" class="manual-btn"></label>
+            <label for="radio2" class="manual-btn"></label>
+            <label for="radio3" class="manual-btn"></label>
+            <label for="radio4" class="manual-btn"></label>
+        </div>
+    </div>
+    <?php
     $noticias = $sistema->getNotic();
     $categorias = $sistema->getCat();
     foreach ($categorias as $categoria):
